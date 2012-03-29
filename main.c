83feb9dab63ff2461297af051bb18415cf06f08b
Pepe Doval <pepellou@gmail.com>
2012-03-29 12:23:59 +0200

    Reducing image size

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
+#include <opencv/cv.h>
 #include <opencv/highgui.h>
 
 CvCapture* g_capture = NULL;
+
+IplImage* doPyrDown(
+	IplImage* in,
+	int filter = IPL_GAUSSIAN_5x5
+) {
+	assert( in->width%2 == 0 && in->height%2 == 0 );
+	IplImage* out = cvCreateImage(
+		cvSize( in->width/2, in->height/2 ),
+		in->depth,
+		in->nChannels
+	);
+	cvPyrDown( in, out );
+	return( out );
+};
 
 int main( int argc, char** argv ) {
 	cvNamedWindow( "KarapaKroma", CV_WINDOW_AUTOSIZE );
@@ -9,7 +24,7 @@ int main( int argc, char** argv ) {
 	while(1) {
 		frame = cvQueryFrame( g_capture );
 		if( !frame ) break;
-		cvShowImage( "KarapaKroma", frame );
+		cvShowImage( "KarapaKroma", doPyrDown(frame) );
 		char c = cvWaitKey(33);
 		if( c == 27 ) break;
 	}
