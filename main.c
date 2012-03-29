07aa3d9e3141e25cb14051b1f0783c1af92b397e
Pepe Doval <pepellou@gmail.com>
2012-03-29 12:19:05 +0200

    Title and global variable

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <opencv/highgui.h>
 
+CvCapture* g_capture = NULL;
+
 int main( int argc, char** argv ) {
-	cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
-	CvCapture* capture = cvCreateCameraCapture( 0 );
+	cvNamedWindow( "KarapaKroma", CV_WINDOW_AUTOSIZE );
+	g_capture = cvCreateCameraCapture( 0 );
 	IplImage* frame;
 	while(1) {
-		frame = cvQueryFrame( capture );
+		frame = cvQueryFrame( g_capture );
 		if( !frame ) break;
-		cvShowImage( "Example2", frame );
+		cvShowImage( "KarapaKroma", frame );
 		char c = cvWaitKey(33);
 		if( c == 27 ) break;
 	}
-	cvReleaseCapture( &capture );
-	cvDestroyWindow( "Example2" );
+	cvReleaseCapture( &g_capture );
+	cvDestroyWindow( "KarapaKroma" );
 }
