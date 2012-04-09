276800dc7cb41de8f307ea471e885e4b4a928dc3
Pepe Doval <pepellou@gmail.com>
2012-04-09 21:21:20 +0200

    Move production code to Chroma class. Refactors must follow.

--- inc/chroma.h
+++ inc/chroma.h
@@ -18,9 +18,32 @@ class Chroma {
 		void setName(std::string name);
 		void setInput(CvCapture *input);
 
+		int thisMethodShouldDie();
+
 	private:
 		std::string _name;
 		CvCapture *_input;
+
+		IplImage *smooth(IplImage *in);
+		IplImage *duplicate(IplImage *in);
+		void putStar(IplImage* frame);
+		void putStarPeak(
+			IplImage* frame,
+			CvPoint pt1,
+			CvPoint pt2,
+			CvPoint pt3,
+			CvPoint pt4,
+			CvScalar color
+		);
+		void fill_croma_from_border(
+			IplImage* frame,
+			CvPoint border_point,
+			CvScalar color,
+			CvScalar threshold = cvScalar(4,4,4)
+		);
+		void invert(IplImage* src, IplImage* dst);
+		void sum_rgb(IplImage* src, IplImage* dst);
+		void MaxCanales(IplImage *color, IplImage *gris);
 
 };
 
